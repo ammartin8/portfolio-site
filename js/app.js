a00f6e22797cb46ac862852585af24341441df3d
ammartin8 <ammartin8249@gmail.com>
2019-07-15 21:37:20 -0400

    added additional images, changed javascript file and added confirmation page

---
+++ js/app.js
@@ -0,0 +1,7 @@
+var confirmit = document.querySelector('.confirm');
+function confirmationresponse() {
+    var div = "";
+    div += "<p>Thank you for contacting me!<br>I will be reaching out to you within the next 48 hours and look forward to connecting with you!</p>";
+    return div;
+};
+confirmit.innerHTML = confirmationresponse();
