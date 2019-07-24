8537a282422e5661797ce02cdcf09b07116db4a8
ammartin8 <ammartin8249@gmail.com>
2019-07-24 19:30:37 -0400

    added alt information to some img tags

--- js/app.js
+++ js/app.js
@@ -1,5 +1,13 @@
 // Provides Thank You Page upon submitting
-$('confirm').html("<p>Thank you for contacting me!<br>I will be reaching out to you within the next 48 hours and look forward to connecting with you!</p>");
+// $('confirm').html("<p>Thank you for contacting me!<br>I will be reaching out to you within the next 48 hours and look forward to connecting with you!</p>");
+var confirmit = document.querySelector('.confirm');
+function confirmationresponse() {
+    var div = "";
+    div += "<p>Thank you for contacting me!<br>I will be reaching out to you within the next 48 hours and look forward to connecting with you!</p>";
+    return div;
+};
+confirmit.innerHTML = confirmationresponse();
+
 
 // Hides rest of blog post
 $('.hidepost').hide();
