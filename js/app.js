ced219bd546ae0fa6ddaf81a8a38d18ac9419eea
ammartin8 <ammartin8249@gmail.com>
2019-07-24 19:33:55 -0400

    changed function that allows additional reading

--- js/app.js
+++ js/app.js
@@ -1,12 +1,6 @@
 // Provides Thank You Page upon submitting
-// $('confirm').html("<p>Thank you for contacting me!<br>I will be reaching out to you within the next 48 hours and look forward to connecting with you!</p>");
-var confirmit = document.querySelector('.confirm');
-function confirmationresponse() {
-    var div = "";
-    div += "<p>Thank you for contacting me!<br>I will be reaching out to you within the next 48 hours and look forward to connecting with you!</p>";
-    return div;
-};
-confirmit.innerHTML = confirmationresponse();
+$('confirm').html("<p>Thank you for contacting me!<br>I will be reaching out to you within the next 48 hours and look forward to connecting with you!</p>");
+
 
 
 // Hides rest of blog post
