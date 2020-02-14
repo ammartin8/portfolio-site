7545d3d95f97e59eb9a9c6e991ae41391f01a967
ammartin8 <ammartin8249@gmail.com>
2020-02-13 22:15:09 -0500

    Change link of landing page project

--- photographyPortfolio/js/app.js
+++ photographyPortfolio/js/app.js
@@ -1,10 +1,10 @@
-$('.confirm').html("<p>Thank you for contacting me!<br>I will be reaching out to you within the next 48 hours and look forward to connecting with you!</p>");
+$('.confirm').html('<p>Thank you for contacting me!<br>I will be reaching out to you within the next 48 hours and look forward to connecting with you!</p>');
 
 // Hides rest of blog post
 $('.hidepost').hide();
 
 // On click shows rest of blog
-$('.readmore').click(function() {
-    $('.hidepost').fadeIn(2000).slideDown();
-    $('.readmore').hide();
+$('.readmore').click(function () {
+  $('.hidepost').fadeIn(2000).slideDown();
+  $('.readmore').hide();
 });
